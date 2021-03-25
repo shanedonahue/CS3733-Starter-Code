8ca4454bb593f4981886c6a77bb4cd37e0ab9527
Craig Dunn <33cjdunn@gmail.com>
2021-03-25 12:43:07 -0400

    Added Name

--- src/main/java/edu/wpi/teamname/Main.java
+++ src/main/java/edu/wpi/teamname/Main.java
@@ -13,5 +13,6 @@ public class Main {
     System.out.println("Matthew Haahr");
     System.out.println("Keith DeSantis");
     System.out.println("Alex Friedman");
+    System.out.println("Craig Dunn");
   }
 }
